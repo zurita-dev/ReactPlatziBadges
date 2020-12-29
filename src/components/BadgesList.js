f3ab997c1918b9ad2bbbc6043302eed6251ad16a
Daniel Zurita <danielzurita94@hotmail.com>
2020-12-28 19:42:50 -0600

    peticion get

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/BadgesList.css';
+import { Link } from 'react-router-dom';
 
 class BadgesList extends React.Component {
   render() {
+    if (this.props.badges.length === 0) {
+      return (
+        <div>
+          <h3>No badges were found</h3>
+          <Link className="btn btn-primary" to="/badges/new">
+            Create new Badge
+          </Link>
+        </div>
+      );
+    }
     return (
       <ul className="list-unstyled">
         {this.props.badges.map((badge) => {
